14ec7c13479c747841ff4d744bbd6adf411238d1
Throuz <zed2786328@gmail.com>
2024-10-31 17:25:03 +0800

    optimize providers

--- app/providers.tsx
+++ app/providers.tsx
@@ -2,43 +2,18 @@
 
 import { ReactNode } from "react";
 import { MetaMaskProvider, MetaMaskSDKOptions } from "@metamask/sdk-react";
-import {
-  isServer,
-  QueryClient,
-  QueryClientProvider,
-} from "@tanstack/react-query";
 
+import { QueryProvider } from "@/components/query-provider";
 import { ThemeProvider } from "@/components/theme-provider";
 
-function makeQueryClient() {
-  return new QueryClient({
-    defaultOptions: {
-      queries: {
-        staleTime: 60 * 1000,
-      },
-    },
-  });
-}
-
-let browserQueryClient: QueryClient | undefined = undefined;
-
-function getQueryClient() {
-  if (isServer) {
-    return makeQueryClient();
-  } else {
-    if (!browserQueryClient) browserQueryClient = makeQueryClient();
-    return browserQueryClient;
-  }
-}
+const sdkOptions: MetaMaskSDKOptions = {
+  dappMetadata: {},
+};
 
 export default function Providers({ children }: { children: ReactNode }) {
-  const sdkOptions: MetaMaskSDKOptions = {
-    dappMetadata: {},
-  };
-  const queryClient = getQueryClient();
   return (
     <MetaMaskProvider sdkOptions={sdkOptions}>
-      <QueryClientProvider client={queryClient}>
+      <QueryProvider>
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
@@ -47,7 +22,7 @@ export default function Providers({ children }: { children: ReactNode }) {
         >
           {children}
         </ThemeProvider>
-      </QueryClientProvider>
+      </QueryProvider>
     </MetaMaskProvider>
   );
 }
